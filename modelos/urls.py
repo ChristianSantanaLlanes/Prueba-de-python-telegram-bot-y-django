f5e0b49bec684e4e98521155de9134e78f84945c
ChristianSantanaLlanes <82761124+ChristianSantanaLlanes@users.noreply.github.com>
2023-02-23 03:42:57 +0000

    ...

--- modelos/urls.py
+++ modelos/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'v1', views.UserViewSet)
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('api/', include(router.urls)),
 ]
--- modelos/views.py
+++ modelos/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from rest_framework import viewsets
+from .serializers import UsuarioSerializer
+from .models import User
 
 # Create your views here.
 
 class Index(TemplateView):
     template_name = "index.html"
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UsuarioSerializer
